150e809dabddf28e5c92a8987440c0b6c3c766ae
Yili Hui <eli970616@lawn-128-61-19-162.lawn.gatech.edu>
2018-09-08 03:15:40 -0400

    Camera working

--- HomeDepot3/ViewController.swift
+++ HomeDepot3/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let pickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        pickerController.sourceType = UIImagePickerControllerSourceType.camera
+        pickerController.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func takePicture(_ sender: Any) {
+        present(pickerController, animated: true, completion: nil)
+        
+    }
+    
 }
 
+extension ViewController : UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("The camera has been closed")
+    }
+}
+
+extension ViewController : UINavigationControllerDelegate {
+    
+}
